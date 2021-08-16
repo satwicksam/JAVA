f11cd643eb9c890bfd63066311e2497f78f5f193
Satwick Sam <satwicksam@gmail.com>
2021-08-16 20:08:20 +0530

    Create GCD.java

---
+++ GCD.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class GCD{
+	public static void main(String args[]){
+		Scanner sc= new Scanner(System.in);
+		System.out.print("Enter two numbers: ");
+		int a= sc.nextInt();
+		int b= sc.nextInt();
+		while(a!=b){
+			if(a>b)
+				a=a-b;
+			else
+				b=b-a;
+		}
+		System.out.print("The GCD is: "+a);
+	}
+}
