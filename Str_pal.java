2172f5c3ea0e5bbff59e4daeac498c96ac023260
Satwick Sam <satwicksam@gmail.com>
2021-08-23 08:35:53 +0530

    Add files via upload

---
+++ Str_pal.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Str_pal{
+	public static void main(String args[]){
+		Scanner s= new Scanner(System.in);
+		System.out.println("Enter the string: ");
+		String st= s.nextLine();
+		char str[]= st.toCharArray();
+		int n= str.length, count=0;
+		for(int i=0; i<n/2; i++){
+			if(str[i]==str[n-(i+1)])
+				count++;
+		}
+		if(count==n/2)
+			System.out.print("Palindrom String!");
+		else
+			System.out.print("Non Palindrom String!");
+	}
+}
