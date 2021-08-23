933414ae854ba5ca8644db5a3b3536d5529c73f1
Satwick Sam <satwicksam@gmail.com>
2021-08-23 08:37:08 +0530

    Update and rename Str_pal.java to String_palindrome.java

--- String_palindrome.java
+++ String_palindrome.java
@@ -1,5 +1,5 @@
 import java.util.*;
-class Str_pal{
+class String_palindrome{
 	public static void main(String args[]){
 		Scanner s= new Scanner(System.in);
 		System.out.println("Enter the string: ");
